1a40bbf57afbfa7c92d8ad23e05726667402cf3a
DarshanKanzariya <kanzariyadarshan24@gamil.com>
2026-01-08 16:01:00 +0530

    Add multiple HTML files and assets for various examples and features

---
+++ HTML/31_timeout.js
@@ -0,0 +1,10 @@
+// Log Start
+console.log("Start");
+
+// setTimeout with 3-second delay
+setTimeout(function () {
+    console.log("This message appears after 3 seconds");
+}, 3000);
+
+// Log End immediately
+console.log("End");
